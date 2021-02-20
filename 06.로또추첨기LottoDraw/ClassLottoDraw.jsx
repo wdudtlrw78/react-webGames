9c9b238f5287d60aee07ad792cb897d0a588966a
momo <wdudtlrw78@gmail.com>
2021-02-20 23:46:36 +0900

    Build class lottoDraw

---
+++ 06.로또추첨기LottoDraw/ClassLottoDraw.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import ClassBall from './ClassBall';
+
+function getWinNumbers() {
+    console.log('getWinNumber');
+    const candidate = Array(45).fill().map((v, i) => i + 1);
+    const shuffle = [];
+    while (candidate.length > 0) {
+        shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1))[0];
+    }
+    const bonusNumber = shuffle[shuffle.length - 1];
+    const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+    return [...winNumbers, bonusNumber];
+}
+
+class ClassLottoDraw extends Component {
+    state = {
+        winNumbers: getWinNumbers(),
+        winBalls: [],
+        bonus: null,
+        redo: false,
+    }
+    
+    timeouts = [];
+
+    runTimeouts = () => {
+        console.log('runTimeouts');
+        const { winNumbers } = this.state;
+        for (let i = 0; i < winNumbers.length - 1; i++) {
+            this.timeouts[i] = setTimeout(() => {
+            this.setState((prevState) => {
+                return {
+                winBalls: [...prevState.winBalls, winNumbers[i]],
+            };
+        });
+        }, (i + 1) * 1000);
+    }
+        this.timeouts[6] = setTimeout(() => {
+        this.setState({
+            bonus: winNumbers[6],
+            redo: true,
+        });
+        }, 7000);
+    };
+
+
+    componentDidMount() {
+        console.log('didmount');
+        this.runTimeouts();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('didupdate');
+        if (this.state.winBalls.length === 0) {
+            this.runTimeouts();
+        }
+
+        if (prevState.winNumbers !== this.state.winNumbers) {
+        console.log('로또 숫자를 생성합니다.');
+    }
+    }
+
+    componentWillUnmount() {
+        this.timeouts.forEach((v) => {
+            clearTimeout(v);
+        })
+    }
+
+    onClickRedo = () => {
+        console.log('onClickRedo');
+        this.setState({
+            winNumbers: getWinNumbers(),
+            winBalls: [],
+            bonus: null,
+            redo: false,
+        });
+        this.timeouts = [];
+    }
+
+    render() {
+        const { winBalls, bonus, redo } = this.state
+        return (
+            <>
+            <div>당첨 숫자</div>
+            <div id="결과창">{winBalls.map((v) => <ClassBall key={v} number={v} />)}</div>
+            <div>보너스!</div>
+            {bonus && <ClassBall number={bonus} />}
+            {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
+            </>
+        )
+}
+}
+
+export default ClassLottoDraw;
---
+++ 06.로또추첨기LottoDraw/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ClassLottoDraw from './ClassLottoDraw';
+
+ReactDOM.render(<ClassLottoDraw />, document.querySelector('#root'))
